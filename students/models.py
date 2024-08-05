db33087b1a3cc5d87ad9db09b374d2aa27f87b0e
Prajwal <im.mht01@gmail.com>
2024-08-05 19:09:29 +0545

    adminpage

--- students/models.py
+++ students/models.py
@@ -9,5 +9,5 @@ class Student(models.Model):
     field_of_study = models.CharField(max_length=50)
     gpa = models.FloatField()
 
-def __str__(self):
-    return f'student: {self.first_name} {self.last_name}'    
+    def __str__(self):
+        return f'Student: {self.first_name} {self.last_name}'    
