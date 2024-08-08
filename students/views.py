2464916fe0e5c691d7500192c101cbf4e0030a12
Prajwal <im.mht01@gmail.com>
2024-08-08 13:04:05 +0545

    Update and Delete

--- students/views.py
+++ students/views.py
@@ -23,7 +23,7 @@ def add(request):
             new_first_name = form.cleaned_data['first_name']
             new_last_name = form.cleaned_data['last_name']
             new_email = form.cleaned_data['email']
-            new_field_of_study = form.cleaned_data['field_of study']
+            new_field_of_study = form.cleaned_data['field_of_study']
             new_gpa = form.cleaned_data['gpa']
 
             new_student =Student(
@@ -44,7 +44,31 @@ def add(request):
         form =StudentForm()
     return render(request, 'students/add.html',{
         'form': StudentForm()
-    })    
+    }) 
+
+def edit(request, id):
+  if request.method == 'POST':
+    student = Student.objects.get(pk=id)
+    form = StudentForm(request.POST, instance=student)
+    if form.is_valid():
+      form.save()
+      return render(request, 'students/edit.html', {
+        'form': form,
+        'success': True
+      })
+  else:
+    student = Student.objects.get(pk=id)
+    form = StudentForm(instance=student)
+  return render(request, 'students/edit.html', {
+    'form': form
+  })
+
+
+def delete(request, id):
+  if request.method == 'POST':
+    student = Student.objects.get(pk=id)
+    student.delete()
+  return HttpResponseRedirect(reverse('index'))
 
 
             
